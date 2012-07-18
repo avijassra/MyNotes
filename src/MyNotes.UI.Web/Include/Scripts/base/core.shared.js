45476918821dc4460f3faa2cebdb96506830df8b
Avi Jassra <avijassra@hotmail.com>
2012-07-18 12:51:09 -0400

    shared functions

--- src/MyNotes.UI.Web/Include/Scripts/base/core.shared.js
+++ src/MyNotes.UI.Web/Include/Scripts/base/core.shared.js
@@ -28,19 +28,42 @@ var mynotes = function () {
             PopupView: '#popupContainer',
             ContentView: '#mainContainer'
         },
-        // methods
+        /*
+            display error alert message
+        */
         displayErrorMessage: function (text) {
             _displayMessage(text, true);
         },
+        /*
+            display success alert message
+        */
         displaySuccessMessage: function (text) {
             _displayMessage(text, false);
         },
+         /*
+            clear alert message
+        */
         clearAlertMessage: function () {
             $(mynotes.constants.AlertMessage).html("").fadeOut('slow', function () { $(this).removeClass('alert alert-error alert-success'); });
         },
+        /*
+            generates unique id
+        */
         uniqid: function () {
-            var newdate = new date;
-            return newdate.gettime();
-        }
+            var newdate = new Date();
+            return newdate.getTime();
+        },
+        /*
+            string formating like c-sharp
+            e.g. stringFormat("{0}, hello world", ['avi']);
+        */
+        stringFormat: function (str, arr) {
+            if (ca4common.isArray(arr)) {
+                for (i in arr) {
+                    str = str.replace('{' + i + '}', arr[i]);
+                }
+            }
+            return str;
+        },
     };
 } ();
